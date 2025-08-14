e80dbe0089975303373af44875783196e3dfca62
RobotChina <17816130706@163.com>
2025-08-14 15:31:32 +0800

    添加清理脚本
    为之后清理模块化做准备

---
+++ cleansystem.bat
@@ -0,0 +1,6 @@
+rmdir /s C:\Users\Robot\AppData\Local\Temp
+rmdir /s C:\Windows\Temp
+rmdir /s C:\Windows\System32\LogFiles
+rmdir /s C:\Windows\Prefetch
+rmdir /s C:\Windows\Help
+cleanmgr.exe /sagerun:1
